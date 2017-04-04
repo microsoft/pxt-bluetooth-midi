1632fb49ddf098547899e93480deea137602fe9d
Peli de Halleux <jhalleux@microsoft.com>
2017-04-03 22:48:43 -0700

    adding note off

--- midi.cpp
+++ midi.cpp
@@ -25,4 +25,16 @@ namespace midi {
     void sendNoteOn(uint8_t channel, uint8_t note, uint8_t velocity) {
         getMidi()->sendNoteOn(channel, note, velocity);
     }
+
+    /**
+     * Send a `Note Off` event
+     * @param channel 0-15
+     * @param note 0-127
+     * @param velocity 0-127
+     */
+    //% block
+    //% channel.min=0 channel.max=15 note.min=0 note.max=127 velocity.min=0 velocity.max=127
+    void sendNoteOff(uint8_t channel, uint8_t note, uint8_t velocity) {
+        getMidi()->sendNoteOff(channel, note, velocity);
+    }
 }
--- shims.d.ts
+++ shims.d.ts
@@ -16,6 +16,16 @@ declare namespace midi {
     //% block
     //% channel.min=0 channel.max=15 note.min=0 note.max=127 velocity.min=0 velocity.max=127 shim=midi::sendNoteOn
     function sendNoteOn(channel: uint8, note: uint8, velocity: uint8): void;
+
+    /**
+     * Send a `Note Off` event
+     * @param channel 0-15
+     * @param note 0-127
+     * @param velocity 0-127
+     */
+    //% block
+    //% channel.min=0 channel.max=15 note.min=0 note.max=127 velocity.min=0 velocity.max=127 shim=midi::sendNoteOff
+    function sendNoteOff(channel: uint8, note: uint8, velocity: uint8): void;
 }
 
 // Auto-generated. Do not edit. Really.
