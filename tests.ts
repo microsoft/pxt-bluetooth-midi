c21cd930a8194331db7948aa88f3d998f96fc223
Peli de Halleux <jhalleux@microsoft.com>
2017-04-03 22:54:34 -0700

    updated example

--- tests.ts
+++ tests.ts
@@ -1,4 +1,5 @@
- // play notes from F#-0 (0x1E) to F#-5 (0x5A):
+// https://www.arduino.cc/en/tutorial/midi
+// play notes from F#-0 (0x1E) to F#-5 (0x5A):
 for (let note = 0x1E; note < 0x5A; note++) {
     //Note on channel 1 (0x90), some note value (note), middle velocity (0x45):
     midi.noteOn(0x90, note, 0x45);
