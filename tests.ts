43130c45e53665b1bb6a214aa9585b0cc102b21d
Peli de Halleux <jhalleux@microsoft.com>
2017-04-23 07:40:03 -0700

    testing channels example

--- tests.ts
+++ tests.ts
@@ -6,14 +6,26 @@ bluetooth.onBluetoothDisconnected(() => {
 })
 bluetooth.startMidiService();
 basic.showString("S")
+let channel = 1;
 input.onButtonPressed(Button.A, () => {
     basic.clearScreen();
-    let piano = midi.inputChannel(0);
+    let piano = midi.inputChannel(channel);
     for (let note = 0x0; note < 0x5A; note++) {
         led.toggle(0, 0);
         piano.noteOn(note);
         basic.pause(100);
         piano.noteOff(note);
         basic.pause(100);
+    }
+})
+input.onButtonPressed(Button.B, () => {
+    channel += 1;
+    basic.showNumber(channel);
+})
+input.onButtonPressed(Button.AB, () => {
+    basic.clearScreen();
+    for (let note = 0; note < 127; note++) {
+        led.toggle(1, 1);
+        midi.playDrum(note);
     }
 })
